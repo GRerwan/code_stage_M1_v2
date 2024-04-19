72ad736274f4b4c6f8202dc0742288badaf3c559
Erwan GRONDIN <40010088@co.univ-reunion.fr>
2024-04-19 09:11:48 +0400

    Update all python files

--- code_affichage_graph.py
+++ code_affichage_graph.py
@@ -9,114 +9,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 from functions import datamanip
 
+import time
+import pvlib
+from pvlib.location import Location
+from pandas.plotting import register_matplotlib_converters
 
 
-'''
-#____south africa_______________
-df_durban_irrad = pd.read_csv('data_brute/south_africa/durban_irrad.csv', sep=';', index_col=0)
-
-
-#____seychelles_______________
-df_amitie_irrad = pd.read_csv('data_brute/seychelles/amitie_irrad.csv', sep=';', index_col=0)
-df_anseboileau_irrad = pd.read_csv('data_brute/seychelles/amitie_irrad.csv', sep=';', index_col=0)
-
-
-#____mauritius_______________
-df_mrtbrasdeau_irrad = pd.read_csv('data_brute/mauritius/mrtbrasdeau_irrad.csv', sep=';', index_col=0)
-df_reservetortues_irrad = pd.read_csv('data_brute/mauritius/reservetortues_irrad.csv', sep=';', index_col=0)
-df_uomreduit_irrad = pd.read_csv('data_brute/mauritius/uomreduit_irrad.csv', sep=';', index_col=0)
-df_vacoas_irrad = pd.read_csv('data_brute/mauritius/vacoas_irrad.csv', sep=';', index_col=0)
-
-#____madagascar_______________
-df_antananarivo_irrad = pd.read_csv('data_brute/mada/antananarivo_irrad.csv', sep=';', index_col=0)
-df_diego_irrad = pd.read_csv('data_brute/mada/diego_irrad.csv', sep=';', index_col=0)
-
-#____reunion_______________
-df_aurere_irrad = pd.read_csv('data_brute/reunion/aurere_irrad.csv', sep=';', index_col=0)
-df_braspanonmoreau_irrad = pd.read_csv('data_brute/reunion/braspanonmoreau_irrad.csv', sep=';', index_col=0)
-df_cavernedufour_irrad = pd.read_csv('data_brute/reunion/cavernedufour_irrad.csv', sep=';', index_col=0)
-df_cilaosbrassec_irrad = pd.read_csv('data_brute/reunion/cilaosbrassec_irrad.csv', sep=';', index_col=0)
-df_cilaospiscine_irrad = pd.read_csv('data_brute/reunion/cilaospiscine_irrad.csv', sep=';', index_col=0)
-df_cilaosthermes_irrad = pd.read_csv('data_brute/reunion/cilaosthermes_irrad.csv', sep=';', index_col=0)
-df_craterebory_irrad = pd.read_csv('data_brute/reunion/craterebory_irrad.csv', sep=';', index_col=0)
-df_edfboisdenefles_irrad = pd.read_csv('data_brute/reunion/edfboisdenefles_irrad.csv', sep=';', index_col=0)
-df_edflapossession_irrad = pd.read_csv('data_brute/reunion/edflapossession_irrad.csv', sep=';', index_col=0)
-df_edfsaintandre_irrad = pd.read_csv('data_brute/reunion/edfsaintandre_irrad.csv', sep=';', index_col=0)
-df_edfsaintleu_irrad = pd.read_csv('data_brute/reunion/edfsaintleu_irrad.csv', sep=';', index_col=0)
-df_edfsaintpierre_irrad = pd.read_csv('data_brute/reunion/edfsaintpierre_irrad.csv', sep=';', index_col=0)
-df_leportbarbusse_irrad = pd.read_csv('data_brute/reunion/leportbarbusse_irrad.csv', sep=';', index_col=0)
-df_leportmairie_irrad = pd.read_csv('data_brute/reunion/leportmairie_irrad.csv', sep=';', index_col=0)
-df_marla_irrad = pd.read_csv('data_brute/reunion/marla_irrad.csv', sep=';', index_col=0)
-df_observatoirevolcan_irrad = pd.read_csv('data_brute/reunion/observatoirevolcan_irrad.csv', sep=';', index_col=0)
-df_oldbsrn_irrad = pd.read_csv('data_brute/reunion/oldbsrn_irrad.csv', sep=';', index_col=0)
-df_oldbsrnsec_irrad = pd.read_csv('data_brute/reunion/oldbsrnsec_irrad.csv', sep=';', index_col=0)
-df_pitondesneiges_irrad = pd.read_csv('data_brute/reunion/pitondesneiges_irrad.csv', sep=';', index_col=0)
-df_plaineparcnational_irrad = pd.read_csv('data_brute/reunion/plaineparcnational_irrad.csv', sep=';', index_col=0)
-df_sainterosemairie_irrad = pd.read_csv('data_brute/reunion/sainterosemairie_irrad.csv', sep=';', index_col=0)
-df_saintjosephmairie_irrad = pd.read_csv('data_brute/reunion/saintjosephmairie_irrad.csv', sep=';', index_col=0)
-df_saintlouisjeanjoly_irrad = pd.read_csv('data_brute/reunion/saintlouisjeanjoly_irrad.csv', sep=';', index_col=0)
-df_saintpaulcarat_irrad = pd.read_csv('data_brute/reunion/saintpaulcarat_irrad.csv', sep=';', index_col=0)
-df_urbsrn_irrad = pd.read_csv('data_brute/reunion/urbsrn_irrad.csv', sep=';', index_col=0)
-df_urbsrnsec_irrad = pd.read_csv('data_brute/reunion/urbsrnsec_irrad.csv', sep=';', index_col=0)
-df_urmoufia_irrad = pd.read_csv('data_brute/reunion/urmoufia_irrad.csv', sep=';', index_col=0)
-df_urtampon_irrad = pd.read_csv('data_brute/reunion/urtampon_irrad.csv', sep=';', index_col=0)
-
-
-#____comores_______________
-df_hahaya_irrad = pd.read_csv('data_brute/comores/hahaya_irrad.csv', sep=';', index_col=0)
-df_ouani_irrad = pd.read_csv('data_brute/comores/ouani_irrad.csv', sep=';', index_col=0)
-
-
-
-df_cilaosbrassec_irrad = pd.read_csv('data_brute/reunion/cilaosbrassec_irrad.csv', sep=';', index_col=0)
-df_cilaospiscine_irrad = pd.read_csv('data_brute/reunion/cilaospiscine_irrad.csv', sep=';', index_col=0)
-df_cilaosthermes_irrad = pd.read_csv('data_brute/reunion/cilaosthermes_irrad.csv', sep=';', index_col=0)
-df_craterebory_irrad = pd.read_csv('data_brute/reunion/craterebory_irrad.csv', sep=';', index_col=0)
-df_edfboisdenefles_irrad = pd.read_csv('data_brute/reunion/edfboisdenefles_irrad.csv', sep=';', index_col=0)
-df_edflapossession_irrad = pd.read_csv('data_brute/reunion/edflapossession_irrad.csv', sep=';', index_col=0)
-df_edfsaintandre_irrad = pd.read_csv('data_brute/reunion/edfsaintandre_irrad.csv', sep=';', index_col=0)
-df_edfsaintleu_irrad = pd.read_csv('data_brute/reunion/edfsaintleu_irrad.csv', sep=';', index_col=0)
-'''
-#df_edfsaintpierre_irrad = pd.read_csv('data_brute/reunion/edfsaintpierre_irrad.csv', sep=';', index_col=0)
-#df_leportbarbusse_irrad = pd.read_csv('data_brute/reunion/leportbarbusse_irrad.csv', sep=';', index_col=0)
-
-'trop long pour vendredi'
-#df_leportmairie_irrad = pd.read_csv('data_brute/reunion/leportmairie_irrad.csv', sep=';', index_col=0)
-
-#df_marla_irrad = pd.read_csv('data_brute/reunion/marla_irrad.csv', sep=';', index_col=0)
-#df_observatoirevolcan_irrad = pd.read_csv('data_brute/reunion/observatoirevolcan_irrad.csv', sep=';', index_col=0)
 
 'bizarre'
 #df_oldbsrn_irrad = pd.read_csv('data_brute/reunion/oldbsrn_irrad.csv', sep=';', index_col=0)
 #df_oldbsrnsec_irrad = pd.read_csv('data_brute/reunion/oldbsrnsec_irrad.csv', sep=';', index_col=0)
 
 
-#df_pitondesneiges_irrad = pd.read_csv('data_brute/reunion/pitondesneiges_irrad.csv', sep=';', index_col=0)
-#df_plaineparcnational_irrad = pd.read_csv('data_brute/reunion/plaineparcnational_irrad.csv', sep=';', index_col=0)
-#df_sainterosemairie_irrad = pd.read_csv('data_brute/reunion/sainterosemairie_irrad.csv', sep=';', index_col=0)
-#df_saintjosephmairie_irrad = pd.read_csv('data_brute/reunion/saintjosephmairie_irrad.csv', sep=';', index_col=0)
-#df_saintlouisjeanjoly_irrad = pd.read_csv('data_brute/reunion/saintlouisjeanjoly_irrad.csv', sep=';', index_col=0)
-#df_saintpaulcarat_irrad = pd.read_csv('data_brute/reunion/saintpaulcarat_irrad.csv', sep=';', index_col=0)
-#df_urbsrn_irrad = pd.read_csv('data_brute/reunion/urbsrn_irrad.csv', sep=';', index_col=0)
-#df_urbsrnsec_irrad = pd.read_csv('data_brute/reunion/urbsrnsec_irrad.csv', sep=';', index_col=0)
-df_urmoufia_irrad = pd.read_csv('data_brute/reunion/urmoufia_irrad.csv', sep=';', index_col=0)
-#df_urtampon_irrad = pd.read_csv('data_brute/reunion/urtampon_irrad.csv', sep=';', index_col=0)
-
-#____comores_______________
-#df_hahaya_irrad = pd.read_csv('data_brute/comores/hahaya_irrad.csv', sep=';', index_col=0)
-#df_ouani_irrad = pd.read_csv('data_brute/comores/ouani_irrad.csv', sep=';', index_col=0)
 
 
+#-------------------------------------------------------
+#      Import data  
+#-------------------------------------------------------
+
+df_reunion_plaineparcnational = pd.read_csv('data_brute/reunion/plaineparcnational_irrad.csv', sep=';', index_col=0) #raw data
+df_test = df_reunion_plaineparcnational['2020':'2021'] # selective data
+data_geo = pd.read_csv('data_geo_all_station.csv', sep=';', index_col=0) # geographic data
+df_one_columns = datamanip.one_column_ghi_dhi(df_test) # simplify data
+df_estim_dni = datamanip.estimation_dni(df_one_columns, data_geo, 'plaineparcnational', 'Indian/Reunion') # data with DNI estimation
+df_filter = datamanip.quality_of_bsrn(df_estim_dni) # useful data
 
 
+#-------------------------------------------------------
+#     Show estimate data of DNI in-situ  
+#-------------------------------------------------------
 
-
-
-
-
-
-
-
-
+datamanip.affiche_courbe_dhi_dni_ghi('plaineparnational', df_filter)
 
 
--- import_raw_data.py
+++ import_raw_data.py
@@ -34,89 +34,19 @@ from bs4 import BeautifulSoup
 
 import time
 
+from functions import datamanip
 
+
+
+# link of all SWOI data
 
 url = "https://galilee.univ-reunion.fr/thredds/catalog/dataStations/catalog.html"
 
-def liste_of_link(url):
-    # Début du compteur de temps
-    start_time = time.time()
-    L=[]
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        # Trouver toutes les balises 'a' qui représentent les dossiers
-        folder_links = soup.find_all('a', href=True)
-        # Extraire les noms de dossier des liens
-        Names_of_country_in_SWOI = [folder.text.strip().rstrip('/') for folder in folder_links if folder.text.strip().endswith('/')]
-        # Afficher les noms de dossier
-        print("Names of country in SWOI:",Names_of_country_in_SWOI)
-    else:
-        print("Erreur lors de la requête HTTP :", response.status_code)
-
-    for i in range(len(Names_of_country_in_SWOI)):
-        url_1 = f"https://galilee.univ-reunion.fr/thredds/catalog/dataStations/{Names_of_country_in_SWOI[i]}/catalog.html"
-        link_data=f'https://galilee.univ-reunion.fr/thredds/dodsC/dataStations/{Names_of_country_in_SWOI[i]}'
-        response_1 = requests.get(url_1)
-        if response_1.status_code == 200:
-            soup = BeautifulSoup(response_1.content, 'html.parser')
-            # Trouver toutes les balises 'a' qui représentent les dossiers
-            folder_links = soup.find_all('a', href=True)
-            # Extraire les noms de dossier des liens
-            folder_names = [folder.text.strip().rstrip('/') for folder in folder_links if folder.text.strip().endswith('/')]
-            # Afficher les noms de dossier
-            print("Noms des dossiers :",folder_names)
-        else:
-            print("Erreur lors de la requête HTTP :", response.status_code)
-        sous_liste=[]
-        for j in range(len(folder_names)):
-            url_2 = f"https://galilee.univ-reunion.fr/thredds/catalog/dataStations/{Names_of_country_in_SWOI[i]}/{folder_names[j]}/catalog.html"
-            link_data_1=f'{link_data}/{folder_names[j]}'
-            response_2 = requests.get(url_2)
-            if response_2.status_code == 200:
-                soup = BeautifulSoup(response_2.content, 'html.parser')
-                # Trouver toutes les balises 'a' qui représentent les dossiers
-                folder_links = soup.find_all('a', href=True)
-                # Extraire les noms de dossier des liens
-                folder_years = [folder.text.strip().rstrip('/') for folder in folder_links if folder.text.strip().endswith('/')]
-                # Afficher les noms de dossier
-                print("Available years :",folder_years)
-            else:
-                print("Erreur lors de la requête HTTP :", response.status_code)
-            sous_sous_liste=[]
-            sous_sous_liste.append(folder_names[j])
-            for k in range(len(folder_years)):
-                url_3 = f"https://galilee.univ-reunion.fr/thredds/catalog/dataStations/{Names_of_country_in_SWOI[i]}/{folder_names[j]}/{folder_years[k]}/catalog.html"
-                link_data_2=f'{link_data_1}/{folder_years[k]}'
-                response_3 = requests.get(url_3)
-                if response_3.status_code == 200:
-                    soup_2 = BeautifulSoup(response_3.content, 'html.parser')
-                    # Trouver tous les liens se terminant par ".nc"
-                    nc_links = soup_2.find_all('a', href=lambda href: href and href.endswith('.nc'))
-                    # Extraire les noms des fichiers .nc
-                    nc_files = [nc_link.text.strip() for nc_link in nc_links]
-                    # Afficher les noms des fichiers .nc
-                    print("Fichiers .nc disponibles :")
-                    for nc_file in nc_files:
-                        print(nc_file)
-                else:
-                    print("Erreur lors de la requête HTTP :", response_2.status_code)
-                
-                for l in range(len(nc_files)):
-                    link_data_3=f'{link_data_2}/{nc_files[l]}'
-                    sous_sous_liste.append(f'{link_data_3}')
-            sous_liste.append(sous_sous_liste)
-        L.append(sous_liste)
-    # Fin du compteur de temps
-    end_time = time.time()
-    # Calcul du temps écoulé
-    execution_time = end_time - start_time
-    print("Temps d'exécution:", execution_time, "secondes")
-    return L
 
 
+liste_of_data=datamanip.liste_of_link(url)
 
-liste_of_data=liste_of_link(url)
+
 # data of South Africa
 south_africa = liste_of_data[0]
 print(f'Data of South Africa : {south_africa}')
@@ -138,13 +68,21 @@ mada = liste_of_data[3]
 print("\n")
 print(f'Data of Madagascar : {mada}')
 
+# data of Comores
+comores = liste_of_data[5]
+print("\n")
+print(f'Data of Comores : {comores}')
+
+
 # data of La Réunion
 reunion = liste_of_data[4]
 print("\n")
 print(f'Data of Reunion : {reunion}')
 
-# data of Comores
-comores = liste_of_data[5]
-print("\n")
-print(f'Data of Comores : {comores}')
+
+# show curve of data
+
+name_station_12, df_station_12 = datamanip.affiche_data(reunion[3])
+print(f'Station of {name_station_12}')
+
 
