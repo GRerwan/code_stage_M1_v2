475590fced5e08ef195bbd063076da5e07a716c2
GRONDIN Erwan <40010088@co.univ-reunion.fr>
2024-04-26 08:49:47 +0400

    Add Useful function part on datamanip.py

--- functions/datamanip.py
+++ functions/datamanip.py
@@ -545,7 +545,9 @@ def count_nan(df):
     return count
 
 
-
+#==============================================================================
+# Creat columns with DNI estimate
+#==============================================================================
 def dni_data(df, data_geo):
     '''
     Return a dataframe with DNI data
@@ -840,4 +842,326 @@ def data_geo_adaption(data_geo, url):
     data_geo_modif['Longitude_modif'] = data_geo_modif['Longitude'].apply(approched_longitude)
     return data_geo_modif
 
+
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+#______________ Useful data ______________________________________________
+#
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+
+import warnings
+from plotly.subplots import make_subplots
+
+# Désactiver l'affichage des FutureWarnings temporairement
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
+def daily_mean(df):
+    df.index = pd.to_datetime(df.index)
+    data = df.copy()
+    # Group data by hour and calculate the mean
+    df_daily_mean = data.resample('D').mean()
+    return df_daily_mean
+
+def mounthly_mean(df):
+    df.index = pd.to_datetime(df.index)
+    data = df.copy()
+    # Group data by hour and calculate the mean
+    df_mounthly_mean = data.resample('M').mean()
+    return df_mounthly_mean
+
+def yearly_mean(df):
+    df.index = pd.to_datetime(df.index)
+    data = df.copy()
+    # Group data by hour and calculate the mean
+    df_yearly_mean = data.resample('Y').mean()
+    return df_yearly_mean
+
+def show_data_useful(name, df):
+    """
+    Plot the irradiance curves (DHI, DNI and GHI) for a given station using Plotly.
+
+    Args:
+        name (str): Name of the station.
+        df (DataFrame): DataFrame containing irradiance valuable data.
+        
+    Returns:
+        None
+
+    Example:
+        name = 'durban'
+        df = df_durban_irrad_useful = pd.read_csv('data_utile/south_africa/durban_irrad_useful.csv', sep=';', index_col=0)
+    """
+    # Convert the index to datetime
+    #df.index = pd.to_datetime(df.index)
+    
+    # Create the figure
+    fig = go.Figure(layout=dict(width=1000, height=600))
+    
+    # Add traces for each irradiance type
+    fig.add_trace(go.Scatter(x=df.index, y=df['dni'], mode='lines', name='DNI',line=dict(width=1)))
+    fig.add_trace(go.Scatter(x=df.index, y=df['ghi'], mode='lines', name='GHI',line=dict(width=1)))
+    fig.add_trace(go.Scatter(x=df.index, y=df['dhi'], mode='lines', name='DHI',line=dict(width=1)))
+    
+    # Customize layout
+    fig.update_layout(title=f'DHI, DNI and GHI in {name}',xaxis_title='Time [UTC+4]',yaxis_title='Irradiance [W/m^2]')
+    
+    # Show the plot
+    fig.show()
+    
+    return
+
+def regroup_sarah_data_mounthly_mean(liste_of_file_path,data_geo):
+    """
+    Save the pixel value on dataframe with SARAH-3 data at netCDF files
+
+    Args:
+        liste_of_file_path (lst) : liste of path data
+        data_geo (dataFrame) : dataframe which contains all geographical data of stations
+
+    Returns:
+        data_geo (dataFrame) : dataframe which contains all geographical data of stations
+
+    Example :
+        liste_of_file_path = ["data_sarah3/2020/DNImm202001010000004UD1000101UD.nc",....,"data_sarah3/2020/DNImm202012010000004UD1000101UD.nc",]
+        data_geo = pd.read_csv('data_geo_adaptation.csv', sep=';', index_col=0)
+    """
+
+    # make a copy to be able to manipulate the dataframe without modifying the original
+    data_modif = data_geo.copy()
+    index_data_geo = data_geo.index.tolist()
+    df = pd.DataFrame(columns=index_data_geo)
+    for i in range(len(liste_of_file_path)):
+        url = liste_of_file_path[i]
+        ncfile = nc.Dataset(url)
+        STANDARD_NAME = "surface_direct_along_beam_shortwave_flux_in_air" #DNI
+        # Searching for variable by standard name
+        varname = next((name for name, var in ncfile.variables.items() if hasattr(var, 'standard_name') and var.standard_name == STANDARD_NAME), None)
+        if not varname:
+            print(f"Error: Unable to find variable with standard name '{STANDARD_NAME}' in file: {url}")
+            ncfile.close()
+            sys.exit(1)
+        # Extracting parameters
+        latitude = ncfile.variables['lat'][:]
+        longitude = ncfile.variables['lon'][:]
+        
+        # Extracting time
+        time_unix = ncfile.variables['time'][:]
+        var_array = np.array(ncfile.variables[varname][:])
+        
+        # Converting time to human-readable format
+        time_array = np.array([datetime.fromtimestamp(t) for t in time_unix])
+        dni_value_sarah = var_array[0]
+        dni_liste = []
+        liste_latitude = list(latitude.data)
+        liste_longitude = list(longitude.data)
+        for i in range(data_modif.shape[0]):
+            float64_number_1 = data_modif['Latitude_modif'][i]
+            float32_number_1 = float64_number_1.astype(np.float32)
+            float64_number_2 = data_modif['Longitude_modif'][i]
+            float32_number_2 = float64_number_2.astype(np.float32)
+            indice_lat = liste_latitude.index(float32_number_1)
+            indice_long = liste_longitude.index(float32_number_2)
+            val = dni_value_sarah[indice_lat][indice_long]
+            dni_liste.append(val)
+        dni_value = dni_liste
+        index = pd.to_datetime(ncfile.time_coverage_start)
+        df.loc[index] = dni_value
+    # Renommer toutes les colonnes
+    new_column_names = [col + '_DNI_SARAH_3' for col in df.columns]
+    # Renommer les colonnes
+    df.rename(columns=dict(zip(df.columns, new_column_names)), inplace=True)
+    df.name = 'DNI_SARAH_3'
+    return df
+
+def show_irrad_all_stations(df_data):
+    """
+    Show mean value of irradiance data for each SWOI stations with Scatter plot
+
+    Args:
+        df_data (DataFrame): DataFrame containing mean irradiance data.
+        
+    Returns:
+        None
+    """
+    df = df_data.copy()
+
+    # Create the figure
+    fig = go.Figure(layout=dict(width=1000, height=600))
+
+    # Ajouter chaque colonne à la figure
+    for column in df.columns:
+        fig.add_trace(go.Scatter(x=df.index, y=df[column], mode='lines', name=column,line=dict(width=1)))
+    
+    
+    # Customize layout
+    fig.update_layout(title='GHI, DHI and DNI',xaxis_title='Time [UTC+4]',yaxis_title='Irradiance [W/m^2]')
+    
+    # Show the plot
+    fig.show()
+    return
+
+def compar_data(liste_of_data, stations_names, start , end, function_mean, variable):
+    """
+    Regroup in-situe data in one dataframe to compare with SARAH data
+
+    Args:
+        liste_of_data (lst): liste of dataFrame containing irradiance data in-situe.
+        stations_names (lst): liste of in-situe name stations.
+        start (str) : start time of comparison.
+        end (str) : finish time of comparison.
+        function_mean (function): mean function you use.
+        variable (str) : the name of comparative variable.
+        
+    Returns:
+        df (dataframe) : dataframe contains all data stations.
+        
+    Example:
+        liste_of_useful_data = [df_durban_irrad_useful,...,df_hahaya_irrad_useful]
+        stations_names = ['durban',...,'hahaya']
+        stations_names = data_geo.index.tolist()
+        function_mean = mounthly_mean
+        start = '2020-01-01'
+        end = '2021-01-01'
+        variable = 'dni'
+    """
+    L = len(liste_of_data)
+    df = pd.DataFrame()
+    for i in range(L):
+        print(f'{i}/{L}') # turn indicator 
+        mean_data = liste_of_data[i].apply(function_mean) # this row permit to apply the mean on the data
+        select_data = mean_data[start:end] # this row permit to select the time zone useful
+        df[stations_names[i]+f'_{variable.upper()}_in_situe'] = select_data[variable] # this row permit to upload each data stations in new dataframe
+    df.index = pd.to_datetime(df.index) # conversion of index
+    # Définir le nom du DataFrame
+    df.name = variable.upper()+'_in_situe'
+    return df
+
+def show_diff_2_data(data_sarah,data_in_situe):
+    """
+    Show mean value of irradiance data for each SWOI stations with Scatter plot (in-situe and SARAH)
+
+    Args:
+        data_sarah (DataFrame): DataFrame containing mean irradiance data of SARAH.
+        data_in_situe (DataFrame): DataFrame containing mean irradiance data in-situe.
+        
+    Returns:
+        None
+        
+    Example:
+        data_sarah = regroup_sarah_data_mounthly_mean(liste_of_file_path,data_geo)
+        data_in_situe = compar_data(liste_of_data, stations_names, start , end, function_mean, variable)
+    """
+    df = data_sarah.copy()
+    df_1 = data_in_situe.copy()
+    df_1.index = df.index
+
+    # Create the figure
+    fig = go.Figure(layout=dict(width=1000, height=600))
+
+    # Ajouter chaque colonne à la figure
+    for column in df.columns:
+        fig.add_trace(go.Scatter(x=df.index, y=df[column], mode='lines', name=column,line=dict(width=1,color='red')))
+    # Ajouter chaque colonne à la figure
+    for column in df_1.columns:
+        fig.add_trace(go.Scatter(x=df.index, y=df_1[column], mode='lines', name=column,line=dict(width=1,color='blue')))
+        #fig.add_trace(go.Scatter(x=df.index, y=df_1[column], mode='lines', name=column,line=dict(width=1, dash='dash'),color='blue'))
+
+    # Customize layout
+    fig.update_layout(title='GHI, DHI and DNI',xaxis_title='Time [UTC+4]',yaxis_title='Irradiance [W/m^2]')
+    
+    # Show the plot
+    fig.show()
+    return
+
+
+def show_irrad_all_stations_Heatmap(df_data):
+    """
+    Show mean value of irradiance data for each SWOI stations
+
+    Args:
+        df_data (DataFrame): DataFrame containing mean irradiance data.
+        
+    Returns:
+        None
+    """
+    df = df_data.copy()
+
+    # Transposer le DataFrame
+    df_transposed = df.transpose()
+
+    # Create the figure
+    fig = go.Figure(layout=dict(width=1000, height=600))
+
+    # Create colormesh
+    fig.add_trace(go.Heatmap(z=df_transposed.values, x=df_transposed.columns, y=df_transposed.index,  colorscale='Viridis'))
+
+    # Customize layout
+    fig.update_layout(title=f'{df_data.name}', xaxis_title='Time [UTC+4]', yaxis_title='Irradiance [W/m^2]')
+
+    # Set y-axis tickmode to 'linear' and specify dtick
+    fig.update_yaxes(tickmode='linear', dtick=1)  # Set the dtick value to adjust spacing between elements
+
+    # Show the plot
+    fig.show()
+
+    return
+
+
+def show_irrad_all_stations_Heatmap_2_data(df_sarah, df_in_situe):
+    """
+    Show mean value of irradiance data for each SWOI stations with Heatmap plot
+
+    Args:
+        df_sarah (DataFrame): DataFrame containing mean irradiance data of SARAH.
+        df_in_situe (DataFrame): DataFrame containing mean irradiance data in-situe.
+        
+    Returns:
+        None
+
+    Example:
+        data_sarah = regroup_sarah_data_mounthly_mean(liste_of_file_path,data_geo)
+        data_in_situe = compar_data(liste_of_data, stations_names, start , end, function_mean, variable)
+    """
+    df1 = df_sarah.copy()
+    df2 = df_in_situe.copy()
+    df1.index = df2.index
+
+    # Transposer les DataFrames
+    df_transposed1 = df1.transpose()
+    df_transposed2 = df2.transpose()
+
+    # Créer les colormesh pour chaque DataFrame
+    trace1 = go.Heatmap(z=df_transposed1.values, x=df_transposed1.columns, y=df_transposed1.index, colorscale='Agsunset')
+    trace2 = go.Heatmap(z=df_transposed2.values, x=df_transposed2.columns, y=df_transposed2.index, colorscale='Agsunset')
+
+    # Créer une disposition avec des subplots empilés verticalement
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.1, subplot_titles=[df_sarah.name, df_in_situe.name])
+
+    # Ajouter les colormesh aux subplots
+    fig.add_trace(trace1, row=1, col=1)
+    fig.add_trace(trace2, row=2, col=1)
+
+    # Personnaliser la mise en page de la figure
+    fig.update_layout(title="Irradiance Comparison", height=1200, showlegend=False, paper_bgcolor='white', xaxis=dict(showgrid=True), yaxis=dict(showgrid=False))
+
+    # Définir les titres des axes pour chaque subplot
+    fig.update_yaxes(title_text="Stations SWOI", row=1, col=1)
+    fig.update_yaxes(title_text="Stations SWOI", row=2, col=1)
+    fig.update_xaxes(title_text="Time [UTC+4]", row=2, col=1)
+
+    # Set y-axis tickmode to 'linear' and specify dtick
+    fig.update_yaxes(tickmode='linear', dtick=1)  # Set the dtick value to adjust spacing between elements
+    # Adjust font size of y-axis tick labels
+    fig.update_yaxes(tickfont=dict(size=10))  # Adjust the size value according to your preference
+
+
+    fig.show()
+
+    return
 
